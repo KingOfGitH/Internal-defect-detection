e6959c9afc260f30154680ce70aecd1125d2c7a5
KingOfGitH <2181820@mail.dhu.edu.cn>
2019-07-08 23:02:10 +0800

    图像处理

---
+++ tuxiangchuli/mat/TEST.cpp
@@ -0,0 +1,318 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <stdio.h>
+//#include <tchar.h>
+//#include <fstream>
+//#define CV_VERSION_ID       CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
+//#ifdef _DEBUG
+//#define cvLIB(name) "opencv_" name CV_VERSION_ID "d"
+//#else
+//#define cvLIB(name) "opencv_" name CV_VERSION_ID
+//#endif
+//#pragma comment( lib, cvLIB("core") )
+//#pragma comment( lib, cvLIB("imgproc") )
+//#pragma comment( lib, cvLIB("highgui") )
+//#pragma comment( lib, cvLIB("flann") )
+//#pragma comment( lib, cvLIB("features2d") )
+//#pragma comment( lib, cvLIB("calib3d") )
+//#pragma comment( lib, cvLIB("gpu") )
+//#pragma comment( lib, cvLIB("legacy") )
+//#pragma comment( lib, cvLIB("ml") )
+//#pragma comment( lib, cvLIB("objdetect") )
+//#pragma comment( lib, cvLIB("ts") )
+//#pragma comment( lib, cvLIB("video") )
+//#pragma comment( lib, cvLIB("contrib") )
+//#pragma comment( lib, cvLIB("nonfree") )
+ using namespace cv;
+ using namespace std;
+
+
+
+ //atan2函数
+Mat cvAtan2Mat(Mat a, Mat b)
+{
+    int rows = a.rows;
+    int cols = a.cols;
+    Mat out;
+	out.create(rows, cols, CV_64FC1);
+   // for (int i=0;i<rows;i++)  
+   // {
+   //     for (int j=0;j<cols;j++)  
+   //     {
+			//double ptra=a.at<double>(i,j);
+			//double ptrb=b.at<double>(i,j);
+			//double ptrout=atan2(ptra,ptrb);
+			////cout<<"ptrout="<<ptrout<<endl;
+			//out.at<double>(i,j)=ptrout;    //修改灰度值  
+   //     }  
+   // }    
+	{  
+		// 取源图像的指针  
+		const double* ptra = a.ptr<double>(0); 
+		const double* ptrb = b.ptr<double>(0);
+		// 将输出数据指针存放输出图像  
+		double* ptrout = out.ptr<double>(0);  
+		for(int j=0; j < cols*rows; j++)  
+		{  
+			ptrout[j]= cv::saturate_cast<double>(atan2(ptra[j],ptrb[j]));  
+		}  
+	} 
+	return out;
+    
+}
+
+//c=0,sin函数;c=1,cos函数
+Mat cvSinMat(Mat a,int c)
+{
+    int rows = a.rows;
+    int cols = a.cols;
+    Mat out;
+	out.create(rows, cols, CV_64FC1);      
+	{  
+		// 取源图像的指针  
+		const double* ptra = a.ptr<double>(0); 
+		// 将输出数据指针存放输出图像  
+		double* ptrout = out.ptr<double>(0);  
+		for(int j=0; j < cols*rows; j++)  
+		{
+			if(c==0)
+			ptrout[j]= cv::saturate_cast<double>(sin(ptra[j]));
+			else ptrout[j]= cv::saturate_cast<double>(cos(ptra[j]));
+		}  
+	} 
+	return out;
+    
+}
+
+//angle函数
+Mat cvAngleMat(Mat a)
+  {
+	Mat realf=cvSinMat(a,1);
+	Mat imagf=cvSinMat(a,0);
+	Mat out=cvAtan2Mat(imagf,realf);
+	return out;
+  }
+
+ //滤波函数
+Mat cvFilMat(Mat a)
+  {
+
+	Mat realf=cvSinMat(a,1);
+	Mat imagf=cvSinMat(a,0);
+	for (int i=0;i<10;i++)
+	{
+	//medianBlur(cvCosMat(wrapped_phase),realf,3);//中值滤波
+	//medianBlur(cvSinMat(wrapped_phase),imagf,3);
+	blur(realf,realf,Size(7,7));//均值滤波
+	blur(imagf,imagf,Size(7,7));
+	}
+	Mat out=cvAtan2Mat(imagf,realf);
+	return out;
+  }
+
+//dx差分,c=0：第1行减第0行，放在第0行，依此类推，最后一行全为0；
+//c=1：第1行减第0行，放在第1行，依此类推，首行全为0；
+Mat cvdxMat(Mat a,int c)
+{
+	int rows = a.rows;
+    int cols = a.cols;
+	Mat out=Mat::zeros(rows, cols, CV_64FC1);
+	for(int i = c; i < rows-1+c; i++)       
+	{  
+		// 取源图像的指针  
+		const double* ptra = a.ptr<double>(i-c); 
+		const double* ptrb = a.ptr<double>(i+1-c);
+		// 将输出数据指针存放输出图像  
+		double* ptrout = out.ptr<double>(i);  
+		for(int j=0; j < cols; j++)  
+		{  
+			ptrout[j]= cv::saturate_cast<double>(ptrb[j]-ptra[j]);  
+		}  
+	} 
+	return out;
+}
+
+//dy差分，c=0：第1列减第0列，放在第0列，依此类推，最后一列全为0；
+//c=1：第1列减第0列，放在第1列，依此类推，首列全为0；
+Mat cvdyMat(Mat a,int c)
+{
+	int rows = a.rows;
+    int cols = a.cols;
+	Mat out=Mat::zeros(rows, cols, CV_64FC1);
+	for(int i = 0; i < rows; i++)       
+	{  
+		// 取源图像的指针   
+		const double* ptra = a.ptr<double>(i);
+		// 将输出数据指针存放输出图像  
+		double* ptrout = out.ptr<double>(i);  
+		for(int j=c; j < cols-1+c; j++)  
+		{  
+			ptrout[j]= cv::saturate_cast<double>(ptra[j+1-c]-ptra[j-c]);  
+		}  
+	} 
+	return out;
+}
+
+////遍历解包裹
+//Mat cvUnwrapMat(Mat a)
+//{
+//	int rows = a.rows;
+//    int cols = a.cols;
+//	for (int j=1;j<cols-1;j++)
+//	{
+//		double ptra =a.at<double>(1,j);
+//		double ptrb =a.at<double>(1,j+1);
+//		if (abs(ptra-ptrb)>=3.14)
+//		{
+//			if (ptra<ptrb)
+//			{
+//				for (int i=j+1;i<cols;i++)
+//				{
+//					double ptrc=a.at<double>(1,i);
+//					a.at<double>(1,i)=ptrc-2*3.14159;
+//				}
+//			}
+//			else
+//			{
+//				for (int i=j+1;i<cols;i++)
+//				{
+//					double ptrc=a.at<double>(1,i);
+//					a.at<double>(1,i)=ptrc+2*3.14159;
+//				}
+//			}
+//		}
+//	}
+//    for (int j=0;j<cols;j++)  
+//    {
+//        for (int i=0;i<rows-1;i++)  
+//        {
+//			double ptra=a.at<double>(i,j);
+//			double ptrb=a.at<double>(i+1,j);      //修改灰度值  
+//			if (abs(ptra-ptrb)>=3.14)
+//			{
+//			if (ptra<ptrb)
+//			{
+//				for (int x=i+1;x<rows;x++)
+//				{
+//					double ptrc=a.at<double>(x,j);
+//					a.at<double>(x,j)=ptrc-2*3.14159;
+//				}
+//			}
+//			else
+//			{
+//				for (int x=i+1;x<rows;x++)
+//				{
+//					double ptrc=a.at<double>(x,j);
+//					a.at<double>(x,j)=ptrc+2*3.14159;
+//				}
+//			}
+//		}
+//        }  
+//    }
+//	return a;
+//}
+
+//全局解包裹
+Mat cvUnwrap1Mat(Mat a)
+{
+	int rows = a.rows;
+    int cols = a.cols;
+	//离散余弦变换临时变量
+	Mat temp1;
+	temp1.create(rows, cols, CV_64FC1);
+	//离散反余弦临时变量
+	Mat temp2=Mat::zeros(rows, cols, CV_64FC1);
+	//输出量
+	Mat out;
+	out.create(rows, cols, CV_64FC1);
+	//置NaN为0
+	 /*for (int i=0;i<rows;i++)  
+    {
+        for (int j=0;j<cols;j++)  
+        {
+			if (_isnan(a.at<double>(i,j))==1)a.at<double>(i,j)=0;
+		}
+	 }*/
+	 //dx差分,dy差分，二阶偏导
+	 Mat dx1=cvAngleMat(cvdxMat(a,0));
+	 Mat dy1=cvAngleMat(cvdyMat(a,0));
+	 Mat dx2=cvdxMat(dx1,1);
+	 Mat dy2=cvdyMat(dy1,1);
+	 Mat f=dx2+dy2;
+	 //离散余弦变换
+	 dct(f,temp1,CV_DXT_FORWARD);
+	 for (int i=0;i<rows;i++)  
+	 {
+		 // 取源图像的指针   
+		 const double* ptr1 = temp1.ptr<double>(i);
+		 // 将输出数据指针存放输出图像  
+		 double* ptr2 = temp2.ptr<double>(i);
+        for (int j=0;j<cols;j++)  
+        {
+			double k1=2*cos((i+1)*3.14159/rows);
+			double k2=2*cos((j+1)*3.14159/cols);
+			double k=k1+k2-4;
+			ptr2[j]=ptr1[j]/k;
+		}
+	 }
+	 dct(temp2,out,CV_DXT_INVERSE);//离散反余弦
+	 return out;
+}
+
+
+
+
+
+
+ int main()
+{
+	double start = static_cast<double>(getTickCount());
+	std::ios::sync_with_stdio(false);
+ 	// 读入图片
+ 	Mat_<double> img1s=imread("D:\\1.bmp",0);
+	Mat_<double> img2s=imread("D:\\2.bmp",0);
+	Mat_<double> img3s=imread("D:\\3.bmp",0);
+	Mat_<double> img4s=imread("D:\\4.bmp",0);
+	Mat_<double> img5s=imread("D:\\5.bmp",0);
+	Mat_<double> img6s=imread("D:\\6.bmp",0);
+	Mat_<double> img7s=imread("D:\\7.bmp",0);
+	Mat_<double> img8s=imread("D:\\8.bmp",0/*CV_LOAD_IMAGE_GRAYSCALE*/);
+	//图片相减
+	Mat_<double> img86s=img8s-img6s;
+	Mat_<double> img57s=img5s-img7s;
+	Mat_<double> img42s=img4s-img2s;
+	Mat_<double> img13s=img1s-img3s;
+	//求相位角
+	Mat  d_phase2=cvAtan2Mat(img86s,img57s);
+	Mat  d_phase1=cvAtan2Mat(img42s,img13s);
+	Mat  d_phase=d_phase2-d_phase1;
+	Mat  wrapped_phase=cvAngleMat(d_phase);
+	//滤波
+	wrapped_phase=cvFilMat(wrapped_phase);
+	//解包裹
+	Mat unwrapped_phase = cvUnwrap1Mat(wrapped_phase);
+	Mat result;
+	normalize(unwrapped_phase,result,0,255,CV_MINMAX);
+	//imshow("a",unwrapped_phase);
+	imwrite("D:\\12.bmp",result);
+	double time = ((double)getTickCount() - start) / getTickFrequency();
+	cout << "所用时间为：" << time << "秒" << endl;
+	waitKey();
+	system("pause");
+	
+ }
+
+
+
+
+  /*img1s.convertTo(img1s, CV_64FC1);
+	img2s.convertTo(img2s, CV_64FC1);
+	img3s.convertTo(img3s, CV_64FC1);
+	img4s.convertTo(img4s, CV_64FC1);
+	img5s.convertTo(img5s, CV_64FC1);
+	img6s.convertTo(img6s, CV_64FC1);
+	img7s.convertTo(img7s, CV_64FC1);
+	img8s.convertTo(img8s, CV_64FC1);*/
---
+++ xiaplvduibi/test/TEST.cpp
@@ -0,0 +1,237 @@
+﻿#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <Math.h>
+#include <armadillo>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#define CV_VERSION_ID       CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
+#ifdef _DEBUG
+#define cvLIB(name) "opencv_" name CV_VERSION_ID "d"
+#else
+#define cvLIB(name) "opencv_" name CV_VERSION_ID
+#endif
+#pragma comment( lib, cvLIB("core") )
+#pragma comment( lib, cvLIB("imgproc") )
+#pragma comment( lib, cvLIB("highgui") )
+#pragma comment( lib, cvLIB("flann") )
+#pragma comment( lib, cvLIB("features2d") )
+#pragma comment( lib, cvLIB("calib3d") )
+#pragma comment( lib, cvLIB("gpu") )
+#pragma comment( lib, cvLIB("legacy") )
+#pragma comment( lib, cvLIB("ml") )
+#pragma comment( lib, cvLIB("objdetect") )
+#pragma comment( lib, cvLIB("ts") )
+#pragma comment( lib, cvLIB("video") )
+#pragma comment( lib, cvLIB("contrib") )
+#pragma comment( lib, cvLIB("nonfree") )
+
+
+
+using namespace cv;
+using namespace std;
+
+// 下标M.at<float>(i,j)  方法1-1  
+cv::Mat inverseColor1(cv::Mat srcImage)  
+{  
+    cv::Mat tempImage = srcImage.clone();  
+    int row = tempImage.rows;  
+    int col = tempImage.cols;  
+    // 分别对各个通道进行反色处理  
+    for (int i = 0; i < row; i++)  
+    {  
+        for (int j = 0; j < col; j++)  
+        {  
+            tempImage.at<cv::Vec3b>(i, j)[0] = 255 - tempImage.at<cv::Vec3b>(i, j)[0];  
+            tempImage.at<cv::Vec3b>(i, j)[1] = 255 - tempImage.at<cv::Vec3b>(i, j)[1];  
+            tempImage.at<cv::Vec3b>(i, j)[2] = 255 - tempImage.at<cv::Vec3b>(i, j)[2];  
+        }  
+    }  
+    return tempImage;  
+}  
+// 方法1-2 下标M::at<float>(i,j)    
+cv::Mat inverseColor2(cv::Mat srcImage)  
+{  
+    cv::Mat tempImage = srcImage.clone();  
+  
+    int row = tempImage.rows;  
+    // 图像像素行实际的宽度  
+    int step = tempImage.step;  
+    // 直接对像素进行反色处理  
+    for (int i = 0; i < row; i++)  
+    {  
+        for (int j = 0; j < step; j++)  
+        {  
+            tempImage.at<uchar>(i, j) = 255 - tempImage.at<uchar>(i, j);  
+        }  
+    }     
+    return tempImage;  
+}  
+// 方法2  指针遍历Mat::ptr  
+cv::Mat inverseColor3(cv::Mat srcImage)  
+{  
+    cv::Mat tempImage = srcImage.clone();  
+    int row = tempImage.rows;  
+    // 将3通道转换为单通道  
+    int nStep = tempImage.cols * tempImage.channels();  
+    for(int i = 0; i < row; i++)   
+    {  
+        // 取源图像的指针  
+        const uchar* pSrcData = srcImage.ptr<uchar>(i);  
+        // 将输出数据指针存放输出图像  
+        uchar* pResultData = tempImage.ptr<uchar>(i);  
+        for(int j=0; j < nStep; j++)  
+        {  
+            pResultData[j]= cv::saturate_cast<uchar>(255 - pSrcData[j]);  
+        }  
+    }  
+    return tempImage;  
+}  
+// 方法3 使用迭代器MatConstIterator  
+cv::Mat inverseColor4(cv::Mat srcImage)  
+{  
+    cv::Mat tempImage = srcImage.clone();  
+    // 初始化源图像迭代器  
+    cv::MatConstIterator_<cv::Vec3b> srcIterStart  = srcImage.begin<cv::Vec3b>();  
+    cv::MatConstIterator_<cv::Vec3b> srcIterEnd = srcImage.end<cv::Vec3b>();  
+    // 初始化输出图像迭代器  
+    cv::MatIterator_<cv::Vec3b> resIterStart = tempImage.begin<cv::Vec3b>();  
+    cv::MatIterator_<cv::Vec3b> resIterEnd = tempImage.end<cv::Vec3b>();  
+    // 遍历图像反色处理  
+    while( srcIterStart != srcIterEnd )  
+    {  
+        (*resIterStart)[0] = 255 - (*srcIterStart)[0];  
+        (*resIterStart)[1] = 255 - (*srcIterStart)[1];  
+        (*resIterStart)[2] = 255 - (*srcIterStart)[2];  
+        // 迭代器递增  
+        srcIterStart++;  
+        resIterStart++;  
+    }  
+    return tempImage;  
+}  
+  
+// 方法4 改进的指针方法isContinuous  
+cv::Mat inverseColor5(cv::Mat srcImage)  
+{  
+    int row = srcImage.rows;  
+    int col = srcImage.cols;  
+    cv::Mat tempImage = srcImage.clone();  
+    // 判断是否是连续图像，即是否有像素填充  
+    if( srcImage.isContinuous() && tempImage.isContinuous() )  
+    {  
+        row = 1;  
+        col = col * srcImage.rows * srcImage.channels();  
+    }  
+    // 遍历图像的每个像素  
+    for(int i = 0; i < row; i++)  
+    {  
+        const uchar* pSrcData = srcImage.ptr<uchar>(i);  
+        uchar* pResultData = tempImage.ptr<uchar>(i);  
+        for(int j = 0; j < col; j++)  
+        {  
+            *pResultData++ = 255 - *pSrcData++;  
+        }  
+    }  
+    return tempImage;  
+}  
+// 方法5 LUT查表法  
+cv::Mat inverseColor6(cv::Mat srcImage)  
+{  
+    int row = srcImage.rows;  
+    int col = srcImage.cols;  
+    cv::Mat tempImage = srcImage.clone();  
+    // 建立LUT 反色table  
+    uchar LutTable[256];  
+    for (int i = 0; i < 256; ++i)  
+        LutTable[i] = 255 - i;  
+    cv::Mat lookUpTable(1, 256, CV_8U);  
+    uchar* pData = lookUpTable.data;   
+    // 建立映射表  
+    for( int i = 0; i < 256; ++i)  
+        pData[i] = LutTable[i];  
+    // 应用索引表进行查找  
+    cv::LUT(srcImage, lookUpTable, tempImage);  
+    return tempImage;  
+}  
+  
+int main()  
+{  
+    // 装载图像转为灰度图像  
+    cv::Mat srcImage = cv::imread("d://flower3.jpg");  
+    if (!srcImage.data)     
+        return -1;  
+    cv::imshow("srcImage", srcImage);  
+  
+    cv::Mat resultImg1,resultImg2,resultImg3;  
+    cv::Mat resultImg4,resultImg5,resultImg6;  
+  
+    // 测试方法1-1  
+    double tTime;  
+    tTime = (double)getTickCount();  
+    const int nTimes = 100;  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg1 = inverseColor1(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test1-1: "  << tTime << std::endl;  
+    // 测试方法1-2  
+    tTime = (double)getTickCount();  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg2 = inverseColor2(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test1-2: "  << tTime << std::endl;  
+    // 测试方法2  
+    tTime = (double)getTickCount();  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg3 = inverseColor3(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test2: "  << tTime << std::endl;  
+    // 测试方法3  
+    tTime = (double)getTickCount();  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg4 = inverseColor4(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test3: "  << tTime << std::endl;  
+    // 测试方法4  
+    tTime = (double)getTickCount();  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg5 = inverseColor5(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test4: "  << tTime << std::endl;  
+    // 测试方法5  
+    tTime = (double)getTickCount();  
+    for(int i  = 0;  i < nTimes; i++)  
+    {  
+        resultImg6 = inverseColor5(srcImage);  
+    }  
+    tTime= 1000*((double)getTickCount() - tTime)/getTickFrequency();  
+    tTime /= nTimes;  
+    std::cout << "test5: "  << tTime << std::endl;  
+  
+    cv::imshow("resultImg1", resultImg1);  
+    cv::imshow("resultImg2", resultImg2);  
+    cv::imshow("resultImg3", resultImg3);  
+    cv::imshow("resultImg4", resultImg4);  
+    cv::imshow("resultImg5", resultImg5);  
+    cv::imshow("resultImg6", resultImg6);  
+  
+    cv::waitKey(0);  
+    return 0;  
+} 
